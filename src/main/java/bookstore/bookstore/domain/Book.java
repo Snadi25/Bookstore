e81ea154147ad0a33e41f0551e34fe0c7a9bf373
toni.parhiala@hotmail.fi <205200898+Toneyy25@users.noreply.github.com>
2025-09-11 18:02:43 +0300

    Korjauksia

--- src/main/java/bookstore/bookstore/domain/Book.java
+++ src/main/java/bookstore/bookstore/domain/Book.java
@@ -25,14 +25,6 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
